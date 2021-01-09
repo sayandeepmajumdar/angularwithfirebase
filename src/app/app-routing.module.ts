210eedfc14fb73b17fd2b07adce1096de7c41125
Sayandeep Majumdar <smajumdar1993@gmail.com>
2021-01-09 14:07:04 +0530

    initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  { path: 'about', component: AboutUsComponent },
+  { path: '', component: HomeComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
